33f305ce3db937e4d421c162a0081f28c58072d1
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-02 11:11:07 -0500

    import images and added file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
         rules: [{
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
+        }, {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+                loader: 'file-loader',
+            }, ],
         }, ],
     },
 };
